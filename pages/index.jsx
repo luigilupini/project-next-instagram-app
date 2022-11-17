7917866033428938a6e9669e8749609a3ced9b28
Luigi Lupini <luigi.lupini@gmail.com>
2022-11-17 16:54:58 +0200

    Initial commit from Create Next App

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Instagram Clone</title>
+      </Head>
+      <h1>Instagram build</h1>
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  );
+}
