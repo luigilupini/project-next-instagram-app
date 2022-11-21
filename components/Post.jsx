258eec65ef5ecedaaa2bc9d21737b00934f643b1
Luigi Lupini <luigi.lupini@gmail.com>
2022-11-21 16:01:58 +0200

    8# `onSnapshot` to subscribe to 'posts' collection

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+
 import {
   BookmarkIcon,
   HeartIcon,
@@ -6,9 +9,10 @@ import {
   FaceSmileIcon,
   PaperAirplaneIcon,
 } from "@heroicons/react/24/outline";
-import Image from "next/image";
+import { useState } from "react";
 
 export default function Post({ id, username, userImg, img, caption }) {
+  const { data: session } = useSession();
   return (
     <div className="bg-white border rounded shadow-sm my-7">
       {/* header */}
@@ -32,33 +36,36 @@ export default function Post({ id, username, userImg, img, caption }) {
         className="object-cover w-full h-full"
       />
       {/* controls/buttons */}
-      <div className="flex justify-between p-2 py-4">
-        <div className="flex gap-4">
-          <HeartIcon className="controlBtn" />
-          <ChatBubbleOvalLeftEllipsisIcon className="controlBtn" />
-          <PaperAirplaneIcon className="h-6 -rotate-45 controlBtn" />
+      {session && (
+        <div className="flex justify-between p-2 py-4">
+          <div className="flex gap-4">
+            <HeartIcon className="controlBtn" />
+            <ChatBubbleOvalLeftEllipsisIcon className="controlBtn" />
+            <PaperAirplaneIcon className="h-6 -rotate-45 controlBtn" />
+          </div>
+          <BookmarkIcon className="controlBtn" />
         </div>
-        <BookmarkIcon className="controlBtn" />
-      </div>
+      )}
       {/* caption */}
       <p className="p-5 text-sm truncate">
         <span className="mr-1 font-bold">{username}:</span>
         {caption}
       </p>
       {/* comments (from firebase) */}
-
       {/* inputBox */}
-      <form className="flex items-center p-4">
-        <FaceSmileIcon className="h-7" />
-        <input
-          type="text"
-          placeholder="Add a comment here!"
-          className="flex-1 mx-2 placeholder-gray-500 border-none rounded outline-none placeholder:text-sm focus:ring-0 bg-gray-50"
-        />
-        <button className="p-1 text-sm font-semibold text-blue-300 transition-all rounded-md hover:text-blue-500 hover:scale-105">
-          Post
-        </button>
-      </form>
+      {session && (
+        <form className="flex items-center p-4">
+          <FaceSmileIcon className="h-7" />
+          <input
+            type="text"
+            placeholder="Add a comment here!"
+            className="flex-1 mx-2 placeholder-gray-500 border-none rounded outline-none placeholder:text-sm focus:ring-0 bg-gray-50"
+          />
+          <button className="p-1 text-sm font-semibold text-blue-300 transition-all rounded-md hover:text-blue-500 hover:scale-105">
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,33 +1,48 @@
+import { useEffect, useState } from "react";
 import Post from "./Post";
 
-const dummyPosts = [
-  {
-    id: "123",
-    username: "mario",
-    userImg: "/user-mario.png",
-    img: "https://images.unsplash.com/photo-1599409636295-e3cf3538f212?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    caption: "Mamma mia! I am number one.",
-  },
-  {
-    id: "124",
-    username: "mario",
-    userImg: "/user-mario.png",
-    img: "https://images.unsplash.com/photo-1612404730960-5c71577fca11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3VwZXIlMjBtYXJpb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=2070&q=60",
-    caption: "It's-a me, Mario!",
-  },
-];
+/* # `onSnapshot` a reference to the document to listen to.
+Attach a listener for `DocumentSnapshot` events. You can either pass individual onNext & onError callbacks or pass a single observer object with next and error callbacks. Although an onCompletion callback can be provided, it will never be called because the snapshot stream is never-ending. @returns an unsubscribe function that can be called to cancel snapshot listener. */
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+
+// const dummyPosts = [
+//   {
+//     id: "123",
+//     username: "mario",
+//     userImg: "/user-mario.png",
+//     img: "https://images.unsplash.com/photo-1599409636295-e3cf3538f212?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+//     caption: "Mamma mia! I am number one.",
+//   },
+//   {
+//     id: "124",
+//     username: "mario",
+//     userImg: "/user-mario.png",
+//     img: "https://images.unsplash.com/photo-1612404730960-5c71577fca11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3VwZXIlMjBtYXJpb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=2070&q=60",
+//     caption: "It's-a me, Mario!",
+//   },
+// ];
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const collectionRef = collection(db, "posts");
+    const unsubscribe = onSnapshot(
+      query(collectionRef, orderBy("timestamp", "desc")),
+      (snapshot) => setPosts(snapshot.docs)
+    );
+    return () => unsubscribe();
+  }, []);
   return (
     <div>
-      {dummyPosts.map(({ id, username, userImg, img, caption }) => (
+      {posts.map((post) => (
         <Post
-          key={id}
-          id={id}
-          username={username}
-          userImg={userImg}
-          img={img}
-          caption={caption}
+          key={post.id}
+          id={post.data().id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
